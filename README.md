f42f3f5c5420800071115969c7ef15b6f79243be
eduardo45MP <eduardopeixoto45@outlook.com>
2023-08-18 05:12:50 -0300

    release1

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# DSM
-Directory Structure Mapper
+# Directory Structure Mapper
+
+This Python script allows you to map the directory structure of a specified directory and write it to a text file. It provides options to include/exclude hidden directories and includes the source code of specific file types within the structure.
+
+## Features
+
+- Recursively maps the directory structure of a specified directory.
+- Includes the source code of `.py`, `.html`, and `.odt` files within the structure.
+- Option to exclude hidden directories (starting with `.`) from the mapping.
+
+## Usage
+
+1. Clone this repository or copy the provided script to your local environment.
+2. Make sure you have Python installed.
+3. Open a terminal and navigate to the directory containing the script.
+
+To run the script, use the following command:
+
+```bash
+python dsm.py
+```
+
+4. Follow the prompts:
+   - Enter the path of the directory you want to map.
+   - Enter the full path (including filename) of the output text file.
+   - Choose whether to include hidden directories in the mapping (answer with `y` for yes or `n` for no).
+
+## Example
+
+Here's an example of how the script works:
+
+```bash
+python dsm.py
+```
+
+```
+Enter the directory path: ./example_directory
+Enter the full path of the output file: ./output/directory_structure.txt
+Do you want to map hidden directories? (y/n): y
+Mapping completed successfully.
+```
+
+The directory structure and source code (if applicable) will be written to the specified output file.
+
+## Disclaimer
+
+This script is provided as-is and may require modifications to suit your specific use case or directory structure.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
---
+++ dsm.py
@@ -0,0 +1,34 @@
+import os
+
+def write_directory_structure(path, indent="", output_file=None, map_hidden=False):
+    if output_file is None:
+        output_file = open("directory_structure.txt", "w", encoding="utf-8")
+
+    for item in os.listdir(path):
+        if not map_hidden and item.startswith('.'):
+            continue  # Skip hidden directories if not mapped
+        item_path = os.path.join(path, item)
+        if os.path.isdir(item_path):
+            output_file.write(f"{indent}./{item}\n")
+            write_directory_structure(item_path, indent + "\t", output_file, map_hidden)
+        elif os.path.isfile(item_path):
+            output_file.write(f"{indent}./{item}\n")
+            if item.endswith(".py") or item.endswith(".html") or item.endswith(".odt"):
+                output_file.write(f"{indent}\t(source code):\n")
+                try:
+                    with open(item_path, "r", encoding="utf-8") as source_file:
+                        source_lines = source_file.readlines()
+                        for line in source_lines:
+                            output_file.write(f"{indent}\t\t{line}")
+                except UnicodeDecodeError:
+                    output_file.write(f"{indent}\t\tCould not read the file content due to encoding issues.\n")
+
+if __name__ == "__main__":
+    root_directory = input("Enter the directory path: ")
+    output_file_path = input("Enter the full path of the output file: ")
+    map_hidden_dirs = input("Do you want to map hidden directories? (y/n): ").lower() == "y"
+
+    with open(output_file_path, "w", encoding="utf-8") as output_file:
+        write_directory_structure(root_directory, output_file=output_file, map_hidden=map_hidden_dirs)
+
+    print("Mapping completed successfully.")
