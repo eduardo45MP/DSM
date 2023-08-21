cc979ceacfcfaaa4e690e362398b9f5bd5248ea4
eduardo45MP <eduardopeixoto45@outlook.com>
2023-08-21 15:48:43 -0300

    DSM 1.1

--- dsm.py
+++ dsm.py
@@ -4,6 +4,11 @@ def write_directory_structure(path, indent="", output_file=None, map_hidden=Fals
     if output_file is None:
         output_file = open("directory_structure.txt", "w", encoding="utf-8")
 
+    programming_file_types = (".py", ".html", ".odt", ".js", ".json", ".java", ".cpp", ".c", ".css", ".php",
+                              ".rb", ".swift", ".go", ".ts", ".sql", ".pl", ".r", ".sh", ".xml")
+
+    office_file_types = (".doc", ".docx", ".md", ".ppt", ".pptx", ".xls", ".xlsx")
+
     for item in os.listdir(path):
         if not map_hidden and item.startswith('.'):
             continue  # Skip hidden directories if not mapped
@@ -13,7 +18,7 @@ def write_directory_structure(path, indent="", output_file=None, map_hidden=Fals
             write_directory_structure(item_path, indent + "\t", output_file, map_hidden)
         elif os.path.isfile(item_path):
             output_file.write(f"{indent}./{item}\n")
-            if item.endswith((".py", ".html", ".odt", ".js", ".json")):
+            if item.endswith(programming_file_types + office_file_types):
                 output_file.write(f"{indent}\t(source code):\n")
                 try:
                     with open(item_path, "r", encoding="utf-8") as source_file:
@@ -32,4 +37,3 @@ if __name__ == "__main__":
         write_directory_structure(root_directory, output_file=output_file, map_hidden=map_hidden_dirs)
 
     print("Mapping completed successfully.")
-
